7743c6ba8282a99c4442b8907f5b637891bd58b1
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-01-08 16:29:02 -0300

    feat: react navigation

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFocusEffect } from "@react-navigation/native";
+
+import Home from "./src/Home";
+import React from "react";
+import StartPage from "./src/StartPage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="StartPage" screenOptions={{
+        headerStyle: {
+          backgroundColor: "#000000",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "900"
+        }
+      }}>
+        <Stack.Screen name="StartPage" component={StartPage} options={{title: "Welcome"}}/>
+        <Stack.Screen name="Home" component={Home} options={{title: "Inicio"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
