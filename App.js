15c5a416c28581214388ede4c1d396b1bb68ff77
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-01-08 17:17:19 -0300

    chore: configuração das rotas

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { useFocusEffect } from "@react-navigation/native";
 import Home from "./src/Home";
 import React from "react";
 import StartPage from "./src/StartPage";
+import Login from "./src/Login";
 
 const Stack = createNativeStackNavigator();
 
@@ -21,7 +22,8 @@ export default function App() {
         }
       }}>
         <Stack.Screen name="StartPage" component={StartPage} options={{title: "Welcome"}}/>
-        <Stack.Screen name="Home" component={Home} options={{title: "Inicio"}}/>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Home" component={Home} options={{headerBackVisible: false, gestureEnabled: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   )
