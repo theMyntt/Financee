21ca12fee1a0693a685f3516a43ca96ac38124c9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-01-14 17:39:59 -0300

    feat: nova tela de inicio

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,12 +1,28 @@
-import { Text, View, StyleSheet, Image } from "react-native";
-import Graphic from "../img/graphic.png";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function Home() {
   return (
     <View style={styles.container}>
-      <Image source={Graphic} style={styles.image} />
-      <View style={styles.optionsContainer}>
-        <Text style={styles.brandText}>Welcome.</Text>
+      <Text style={styles.brandText}>Welcome back</Text>
+      <View style={styles.card}>
+        <View style={{ padding: 20 }}>
+          <Text style={styles.textWhite}>Your balance:</Text>
+          <Text style={styles.brandText}>USD 00,00</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={{ padding: 20, flexDirection: "row", flexWrap: "wrap" }}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          
+        </View>
       </View>
     </View>
   );
@@ -16,18 +32,33 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#000000",
     flex: 1,
-    // alignItems: "center"
+    paddingHorizontal: 30,
   },
   image: {
     height: "50%",
-    width: "100%"
+    width: "100%",
   },
   brandText: {
     color: "#fff",
     fontSize: 24,
-    fontWeight: "900"
+    fontWeight: "900",
   },
-  optionsContainer: {
-    paddingHorizontal: 30
-  }
+  card: {
+    backgroundColor: "#222222",
+    borderRadius: 20,
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  textWhite: {
+    color: "#fff",
+    fontWeight: "900",
+  },
+  button: {
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 4,
+    padding: 5,
+    marginRight: 10,
+    // marginBottom: 10
+  },
 });
