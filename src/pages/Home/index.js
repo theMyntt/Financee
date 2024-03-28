3667d4380a14f9dd2b41935c7c6dd9c7cc982b57
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-28 14:59:21 -0300

    feat: route params

---
+++ src/pages/Home/index.js
@@ -0,0 +1,33 @@
+import { Text, View, TouchableOpacity } from "react-native";
+import styles from "./index.style"
+import { useRoute } from "@react-navigation/native";
+
+export default function Home() {
+  const route = useRoute();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.brandText}>Welcome back</Text>
+      <View style={styles.card}>
+        <View style={{ padding: 20 }}>
+          <Text style={styles.textWhite}>Your balance:</Text>
+          <Text style={styles.brandText}>USD {route.params.balance}</Text>
+        </View>
+      </View>
+      <View style={styles.card}>
+        <View style={{ padding: 20, flexDirection: "row", flexWrap: "wrap" }}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.textWhite}>Transfer</Text>
+          </TouchableOpacity>
+          
+        </View>
+      </View>
+    </View>
+  );
+}
