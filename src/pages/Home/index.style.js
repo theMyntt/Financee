03e30258747ee51053272acc6d97db0978c867b2
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-03-28 14:59:02 -0300

    chore: unstructed style

---
+++ src/pages/Home/index.style.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#000000",
+    flex: 1,
+    paddingHorizontal: 30,
+  },
+  image: {
+    height: "50%",
+    width: "100%",
+  },
+  brandText: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "900",
+  },
+  card: {
+    backgroundColor: "#222222",
+    borderRadius: 20,
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  textWhite: {
+    color: "#fff",
+    fontWeight: "900",
+  },
+  button: {
+    borderColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 4,
+    padding: 5,
+    marginRight: 10,
+    // marginBottom: 10
+  },
+});
+
+export default styles;
